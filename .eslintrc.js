0f33c9c7ccc11e23c5bc84ef3f6f6a59c3ec4c87
ext.shenxu5 <ext.shenxu5@jd.com>
2022-08-09 23:20:51 +0800

    ✨ feat: recoil状态管理和路由

--- .eslintrc.js
+++ .eslintrc.js
@@ -92,7 +92,14 @@ module.exports = {
     // 检查 Hook 的规则
     'react-hooks/rules-of-hooks': 'error',
     // 检查 effect 的依赖
-    'react-hooks/exhaustive-deps': 'warn',
+    // 'react-hooks/exhaustive-deps': 'warn',
+    // recoiljs
+    'react-hooks/exhaustive-deps': [
+      'warn',
+      {
+        additionalHooks: 'useRecoilCallback',
+      },
+    ],
   },
   settings: {
     'import/resolver': {
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { RecoilRoot } from 'recoil';
+import { BrowserRouter } from 'react-router-dom';
+import App from './app';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </RecoilRoot>
   </React.StrictMode>
 );
